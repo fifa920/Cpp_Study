4d7a19716ef0df2c4a0e285410ba9785b3d67fc9
Desktop_home <Desktop@home.com>
2023-09-15 00:23:04 +0900

    .gitignore 생성 및 적용

--- Chapter8/Chapter8/Chapter8.cpp
+++ Chapter8/Chapter8/Chapter8.cpp
@@ -3,8 +3,32 @@
 
 #include <iostream>
 
+class Fraction
+{
+private:
+    int m_numerator;
+    int m_denominator;
+
+public:
+    Fraction()
+    {
+        m_numerator = 1;
+        m_denominator = 1;
+
+        std::cout << "Fraction() COnstructor" << std::endl;
+    }
+    
+    void print()
+    {
+        std::cout << m_numerator << "/ " << m_denominator << std::endl;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 생성자의 parameter가 없을 때는 frac으로 선언하고
+    // 생성사의 parameter가 있을 떄는 frac() 으로 처리한다.
+    Fraction frac;
+    frac.print();
 }
 
